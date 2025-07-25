3de956b731c5f39dbc89ce4591b51fb29d451599
Muhammed Ebrahim <ebrahimmuhammed479@gmail.com>
2025-07-25 21:08:12 +0300

    Commets in Kotlin

---
+++ src/hello_world/CommentInKotlin.kt
@@ -0,0 +1,28 @@
+package hello_world
+
+// Here’s a complete explanation of comments in Kotlin, from basic to advanced, including how and when
+// to use them effectively.
+
+//  Basic Comments in Kotlin
+
+// for code explanation either for single line or multiline comments
+
+
+// this is KDoc(Kotlin documentation) that is automatically read by the IDE when we hover ove the function or class
+// to explain about it and there are also many keyword annotations describe for ke words like @param(parameter)
+/**
+ * Summary
+ *
+ * | Feature               | Description                                    |
+ * | --------------------- | ---------------------------------------------- |
+ * | `//`                  | Single-line comment                            |
+ * | `/* ... */`           | Multi-line block comment                       |
+ * | Nested block comments | Kotlin supports this (rare in other languages) |
+ * | `/** ... */`          | KDoc for generating documentation              |
+ * | `@param`, `@return`   | Used inside KDoc                               |
+ * | IDE Integration       | IntelliJ recognizes and displays KDoc          |
+ *
+ *
+ *
+ *
+ */
